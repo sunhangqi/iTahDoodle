c00c933df36845a41f96da3f3a547b55a34c1180
SunHangqi <sunhangqi@gmail.com>
2020-09-07 06:36:32 +0800

    AppDelegate添加四个属性以及一个方法

--- iTahDoodle/AppDelegate.h
+++ iTahDoodle/AppDelegate.h
@@ -9,7 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
 
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void)addTasks:(id)sender;
 
 @end
 
